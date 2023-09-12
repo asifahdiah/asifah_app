360d7120d5b18337058124f887b7f918d512ab28
asifahdiah <asifahdiahmanurung13@gmail.com>
2023-09-12 17:29:18 +0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>asifah</title>
+  <link rel="stylesheet" href="styleing.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"'
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body class="container-fluid"><style>
+      html, body {
+      min-height: 100%;
+      }
+      body, div, form, input, select, p { 
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 16px;
+      color: #000000;
+      }
+      body {
+      background: url("/uploads/media/default/0001/01/b5edc1bad4dc8c20291c8394527cb2c5b43ee13c.jpeg") no-repeat center;
+      background-size: cover;
+      }
+      h1, h2 {
+      text-transform: uppercase;
+      font-weight: 400;
+      }
+      h2 {
+      margin: 0 0 0 8px;
+      }
+      .main-block {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      padding: 25px;
+      background: rgba(0, 0, 0, 0.5); 
+      }
+      .left-part, form {
+      padding: 25px;
+      }
+      .left-part {
+      text-align: center;
+      }
+      .fa-graduation-cap {
+      font-size: 72px;
+      }
+      form {
+      background: rgba(0, 0, 0, 0.7); 
+      }
+      .title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+      }
+      .info {
+      display: flex;
+      flex-direction: column;
+      }
+      input, select {
+      padding: 5px;
+      margin-bottom: 30px;
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid #eee;
+      }
+      input::placeholder {
+      color: #eee;
+      }
+      option:focus {
+      border: none;
+      }
+      option {
+      background: black; 
+      border: none;
+      }
+      .checkbox input {
+      margin: 0 10px 0 0;
+      vertical-align: middle;
+      }
+      .checkbox a {
+      color: #26a9e0;
+      }
+      .checkbox a:hover {
+      color: #85d6de;
+      }
+      .btn-item, button {
+      padding: 10px 5px;
+      margin-top: 20px;
+      border-radius: 5px; 
+      border: none;
+      background: #26a9e0; 
+      text-decoration: none;
+      font-size: 15px;
+      font-weight: 400;
+      color: #fff;
+      }
+      .btn-item {
+      display: inline-block;
+      margin: 20px 5px 0;
+      }
+      button {
+      width: 100%;
+      }
+      button:hover, .btn-item:hover {
+      background: #85d6de;
+      }
+      @media (min-width: 568px) {
+      html, body {
+      height: 100%;
+      }
+      .main-block {
+      flex-direction: row;
+      height: calc(100% - 50px);
+      }
+      .left-part, form {
+      flex: 1;
+      height: auto;
+      }
+      }
+    </style>
+    <div class="landingPageWrapper container-fluid" id="home">
+      <header
+        class="container-fluid d-flex justify-content-center align-items-center py-4 shadow-sm border-5 border-top position-fixed"
+      >
+        <section
+          class="container-lg d-flex flex-row justify-content-between align-items-center text-light"
+        >
+          <div>
+            <h1 class="fw-medium">Asifah</h1>
+          </div>
+          <nav class="w-25">
+            <ul class="d-flex justify-content-between">
+              <li class="list-unstyled">
+                <a href="#home" class="text-decoration-none text-light">Home</a>
+              </li>
+              <li class="list-unstyled">
+                <a href="#about" class="text-decoration-none text-light"
+                  >About</a
+                >
+              </li>
+              <li class="list-unstyled">
+                <a href="#book" class="text-decoration-none text-light">Book</a>
+              </li>
+              <li class="list-unstyled">
+                <a href="#faq" class="text-decoration-none text-light">FAQ</a>
+              </li>
+              <li class="list-unstyled">
+                <a href="#faq" class="text-decoration-none text-light">Pesan</a>
+              </li>
+            </ul>
+          </nav>
+          <div>
+            <a target="_blank" href="https://wa.me/+6285668732918?text=hai%20acipeh" class="btn text-light fw-semibold py-2 px-3">Contact</a>
+          </div>
+        </section>
+      </header>
+      <main class="container-fluid">
+        <section class="container-fluid">
+          <video
+            class="container-fluid"
+            autoplay
+            muted
+            loop
+            src="img/0416.mp4"
+          ></video>
+        </section>
+        <section
+          class="heroWrapper container-fluid position-absolute top-0 d-flex justify-content-center align-items-center"
+        >
+          <div class="heroTextWrapper text-center">
+            <h1>Holla , nice to meet you!!</h1>
+            <p class="fs-4">
+              My name is Asifah. Hope you enjoy my website yea!!. keep surfingg.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+    <div
+      class="about container-fluid d-flex justify-content-center align-items-center"
+      id="about"
+    >
+      <div
+        class="row container-fluid justify-content-around align-items-center"
+      >
+        <div class="col-3">
+          <img src="img/Left hander-cuate.svg" alt="" />
+        </div>
+        <div class="col-4">
+          <div>
+            <h1 class="fs-1">Hii </h1>
+            <p>Im a trusted fren u know!! </p>
+            <p class="fs-5 mt-3">
+              My name is Asifah Diah Grasela Manurung, i' m 16 years old and i'm a newbie novel writer. I'm from Medan, Indonesia and i born at Medan. I still second grade of vecational high school named SMK Telkom 1 Medan. And my hobby is writinggg!! </p>
+  <p class="mt-3 muted">
+    For a lot of me. U can follow my
+    <a target="_blank" class="text-decoration-none fw-semibold text-danger" href="https://www.instagram.com/asf_dyzah/">instagram</a>
+
+  </p>
+  </div>
+  </div>
+  </div>
+  </div>
+  <div class="book container-fluid d-flex justify-content-center align-items-center" id="book">
+    <div class="row justify-content-around align-items-center">
+      <div class="col-5">
+        <img class="img-fluid" src="img/1681621097.png" alt="" />
+      </div>
+      <div class="col-5">
+        <div class="text-light">
+          <h1 class="fw-light">my journey with Mark Lee.</h1>
+          <p class="fs-5 mt-1">
+            this book is my first novel that i created and i'll publish it
+            tommorow (*i'm blusshing). I just copy like 300 books.
+          </p>
+          <p class="fs-5 mt-3">
+            oh yea for information : <br />
+            <strong>u all can buy my book at Gramedia of the physical form, and
+              Scribd for the online form.</strong>
+            <br />
+            I recommend the physical form tho!
+          </p>
+          <p class="mt-4">it's cheap, just :</p>
+          <h1 class="fw-bold">Rp75.500</h1>
+          <p class="mt-4">Follow the official instagram account will pay :</p>
+          <h1 class="fw-bold fs-2">
+            <span class="text-danger text-decoration-line-through">Rp75.500</span>
+            <span class="fs-1">Rp50.000</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="faq container-fluid" id="faq">
+    <h1 class="fw-bold text-center">FREQUENTLY ASKED QUESTION!!!</h1>
+    <div class="row justify-content-around align-items-center">
+      <div class="col-4">
+        <img class="w-100" src="img/FAQs.gif" alt="">
+      </div>
+      <div class="col-4 accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button p-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              So r u a kpopers? and what band do u like
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body p-3">
+              <strong>Yes i am!!</strong> Hm hard question by the way, but from my deepest place of my heart, i fall in love with <strong>NCT U KNOW!!!</strong> mark lee 🥰
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed p-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              What major that u choosed when u study at SMK Telkom 1 Medan?
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body p-3">
+              <strong>RPL!!</strong> or for the international termonology : software engineering. Like coding, website, mobile, iot , or something. Little fun yea. Not bat tho.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed p-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Hal yang disukai?
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body p-3">
+              <ol class="ps-4">
+                <li>Writing book</li>
+                <li>Watching One Piece</li>
+                <li>Eating</li>
+                <li>Sleep</li>
+                <li>Playin tiktok, maybe</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed p-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              Fav songgg!?!??
+            </button>
+          </h2>
+          <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body p-3">
+              ABSOLUTELYYY!!! THE ONE AND ONLYY : <strong>Rose by D.O.</strong>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed p-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              What personality that u confirm that being ur real personality?
+            </button>
+          </h2>
+          <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body p-3">
+              <strong></strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="container-fluid p-5">
+    <div class="d-flex flex-row justify-content-start">
+      <div class="text-light d-flex flex-column">
+        <h1>Contact Me</h1>
+        <hr class="border-4 my-2">
+        <p class=" fs-5">Email : asifah@gmail.com</p>
+        <p class=" fs-5">Phone : +628 1234 5678</p>
+      </div>
+      <div class="text-light d-flex flex-column mx-5">
+        <h1>Links</h1>
+        <hr class="border-4 my-2">
+        <a class="text-light text-decoration-none fs-5" href="">Home</a>
+        <a class="text-light text-decoration-none fs-5" href="">About</a>
+        <a class="text-light text-decoration-none fs-5" href="">Book</a>
+        <a class="text-light text-decoration-none fs-5" href="">FAQ</a>
+        <a class="text-light text-decoration-none fs-5" href="">Contact</a>
+      </div>
+      <div class="text-light d-flex flex-column">
+        <h1>Follow Me on</h1>
+        <hr class="border-4 my-2">
+        <p class=" fs-5">instagram : asf_dyzah</p>
+        <p class=" fs-5">youtube : asifah</p>
+        <p class=" fs-5">tiktok : asifah_diah</p>
+        <p class=" fs-5">scribd : asifah</p>
+      </div>
+    </div>
+    <br><br><br><br><br><br><br>
+    <div>
+
+      <div class="main-block">
+        <div class="left-part">
+          <i class="fas fa-graduation-cap"></i>
+          <h1>Have A Book, and be Happy.</h1>
+          <p>Pesan memalui website dan dapatkan potongan hingga 50% </p>
+          <div class="btn-group">
+          </div>
+        </div>
+        <form action="" method="post">
+          <div class="title">
+            <i class="fas fa-pencil-alt"></i>
+            <h2>Order Here</h2>
+          </div>
+          <div class="info">
+            <input class="fname" type="text" name="fullname" placeholder="Full name">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="phonenumber" placeholder="Phone number">
+            <input type="address" name="address" placeholder="Address">
+
+
+
+
+          </div>
+          <button type="submit" href="/" name="submit">Submit</button>
+        </form>
+      </div>
+
+    </div>
+    <br>
+    <p class="text-light text-center fs-5 mt-5">© kita 2023</p>
+  </footer>
+  </body>
+
+</html>
